96883c74b3ab0d7ef081d715c2e6ef94e7fde6d8
JJ Espinoza <jj.espinoza@kw.com>
2023-05-23 11:46:10 -0700

    added a csv data deduplication script

---
+++ dedup_agents.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import os
+import csv
+import re
+import logging
+import pprint
+import optparse
+import json
+
+import dedupe
+from unidecode import unidecode
+
+pp = pprint.PrettyPrinter(width=41, compact=True)
+
+def preProcess(column):
+    """
+    Do a little bit of data cleaning with the help of Unidecode and Regex.
+    Things like casing, extra spaces, quotes and new lines can be ignored.
+    """
+    column = unidecode(column)
+    column = re.sub('  +', ' ', column)
+    column = re.sub('\n', ' ', column)
+    column = column.strip().strip('"').strip("'").lower().strip()
+    # If data is missing, indicate that by setting the value to `None`
+    if not column:
+        column = None
+    return column
+
+
+def readData(filename):
+    """
+    Read in our data from a CSV file and create a dictionary of records,
+    where the key is a unique record ID and each value is dict
+    """
+
+    data_d = {}
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            clean_row = [(k, preProcess(v)) for (k, v) in row.items()]
+            row_id = row['ID']
+            data_d[row_id] = dict(clean_row)
+
+    return data_d
+
+
+if __name__ == '__main__':
+
+    # ## Logging
+
+    # Dedupe uses Python logging to show or suppress verbose output. This
+    # code block lets you change the level of loggin on the command
+    # line. You don't need it if you don't want that. To enable verbose
+    # logging, run `python examples/csv_example/csv_example.py -v`
+    optp = optparse.OptionParser()
+    optp.add_option('-v', '--verbose', dest='verbose', action='count',
+                    help='Increase verbosity (specify multiple times for more)'
+                    )
+    (opts, args) = optp.parse_args()
+    log_level = logging.WARNING
+    if opts.verbose:
+        if opts.verbose == 1:
+            log_level = logging.INFO
+        elif opts.verbose >= 2:
+            log_level = logging.DEBUG
+    logging.getLogger().setLevel(log_level)
+
+    # ## Setup
+
+    input_file = 'agents_with_duplicates.csv'
+    output_file = 'agents_with_no_duplicates.csv'
+    settings_file = 'csv_example_learned_settings'
+    training_file = 'csv_example_training.json'
+
+    print('importing data ...')
+    data_d = readData(input_file)
+
+    # If a settings file already exists, we'll just load that and skip training
+    if os.path.exists(settings_file):
+        print('reading from', settings_file)
+        with open(settings_file, 'rb') as f:
+            deduper = dedupe.StaticDedupe(f)
+    else:
+        # ## Training
+
+        # Define the fields dedupe will pay attention to
+        fields = [
+            {'field': 'FIRST_NAME', 'type': 'String', 'has missing': True},
+            {'field': 'LAST_NAME', 'type': 'String', 'has missing': True},
+            {'field': 'LIST_AGENT_EMAIL', 'type': 'Exact', 'has missing': True},
+            {'field': 'LIST_AGENT_PREFERRED_PHONE', 'type': 'String', 'has missing': True},
+            ]
+
+        # Create a new deduper object and pass our data model to it.
+        deduper = dedupe.Dedupe(fields)
+
+        # If we have training data saved from a previous run of dedupe,
+        # look for it and load it in.
+        # __Note:__ if you want to train from scratch, delete the training_file
+        if os.path.exists(training_file):
+            print('reading labeled examples from ', training_file)
+            with open(training_file, 'rb') as f:
+                deduper.prepare_training(data_d, f)
+        else:
+            deduper.prepare_training(data_d)
+
+        # ## Active learning
+        # Dedupe will find the next pair of records
+        # it is least certain about and ask you to label them as duplicates
+        # or not.
+        # use 'y', 'n' and 'u' keys to flag duplicates
+        # press 'f' when you are finished
+        print('starting active labeling...')
+
+        
+        dedupe.consoleLabel(deduper)
+
+        # Using the examples we just labeled, train the deduper and learn
+        # blocking predicates
+        deduper.train()
+
+        # When finished, save our training to disk
+        with open(training_file, 'w') as tf:
+            deduper.writeTraining(tf)
+
+        # Save our weights and predicates to disk.  If the settings file
+        # exists, we will skip all the training and learning next time we run
+        # this file.
+        with open(settings_file, 'wb') as sf:
+            deduper.writeSettings(sf)
+
+    # ## Clustering
+
+    # `partition` will return sets of records that dedupe
+    # believes are all referring to the same entity.
+
+    print('clustering...')
+    clustered_dupes = deduper.match(data_d,0.5)
+    
+
+
+    print('# duplicate sets', len(clustered_dupes))
+
+    # ## Writing Results
+
+    # Write our original data back out to a CSV with a new column called
+    # 'Cluster ID' which indicates which records refer to each other.
+
+    cluster_membership = {}
+    for cluster_id, (records, scores) in enumerate(clustered_dupes):
+        for record_id, score in zip(records, scores):
+            cluster_membership[record_id] = {
+                "Cluster ID": cluster_id,
+                "confidence_score": score
+            }
+    
+
+    # pp.pprint(cluster_membership)
+    with open('cluster_membership.json', 'w', encoding='utf-8') as f:
+        json.dump(str(cluster_membership), f, ensure_ascii=False, indent=4)
