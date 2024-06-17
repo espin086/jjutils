3d215055a07b55cd2a8f63cfa7234a1913338f1f
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2024-06-16 20:04:11 -0700

    added gitignore and cleaned up the repo

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 build:
-	#rm -r build dist 
-	python3 -m build --sdist --wheel ./
+	rm -r build dist &  python3 -m build --sdist --wheel ./
 
 test:
 	pytest
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
+
     requirements = f.read().splitlines()
 
 setup(
     name="jjutils",
-    version="0.0.10",
+    version="0.0.11",
     packages=find_packages(),
     install_requires=requirements,
 )
